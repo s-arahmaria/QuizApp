6afe407579fde759a727fa7a079c52ac9304e639
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-05-13 16:00:57 +0100

    add HomeScreen with logo, topic cards and icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-core:1.6.0")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
